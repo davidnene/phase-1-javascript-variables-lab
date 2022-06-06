7ec2475228db07db35f56bec5d0dae47a560310e
davidnene <davienesh4@gmail.com>
2022-06-06 21:47:21 +0300

    commit tree 0fa870d9fbf9b9b82a1b804ef2ef7f1fc17e82c7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+companyName == 'scuber'
+const companyName = 'Scuber'
+
+mostProfitableNeighborhood == 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea'
+
+companyCeo == 'Susan Smith'
+let companyCeo = 'Susan Smith'
